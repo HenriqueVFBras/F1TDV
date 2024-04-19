3a5e3812b7350ea4481e9c6e7fd723fa6ded5428
GabrielLima00 <152295854+GabrielLima00@users.noreply.github.com>
2024-04-19 14:07:02 +0100

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace MonoPong
+{
+    public class Pong : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        public static Vector2 originalScreenSize;
+        private  Vector2 _setScreenSize;
+        private Matrix _globalTransformation;
+        private int _backbufferWidth;
+        private int _backbufferHeight;
+        private Score _score;
+        private GamingTimer _gamingTimer;
+        private Menu _menu;
+        private FrameCounter _frameCounter;
+        private List<PongSprite> _pongSprites;
+        public static Random random;
+        public static bool? BattelComputer;
+        public static Ball ball;
+
+        public Pong()
+        {
+            this._graphics = new GraphicsDeviceManager(this);
+            this.Content.RootDirectory = "Content";
+            this.IsMouseVisible = true;
+            originalScreenSize = new Vector2(800, 480);
+            this._setScreenSize = new Vector2(1280, 720);
+            random = new Random();
+        }
+
+        private void InitializeScreenSize()
+        {
+          this._graphics.IsFullScreen = false;
+          this._graphics.PreferredBackBufferWidth = (int)this._setScreenSize.X;
+          this._graphics.PreferredBackBufferHeight = (int)this._setScreenSize.Y;
+          this._graphics.ApplyChanges();
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            this.InitializeScreenSize();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            var batTexture = Content.Load<Texture2D>("Bat");
+            var ballTexture = Content.Load<Texture2D>("Ball");
+            var backGround = Content.Load<Texture2D>("Background");
+            this._spriteBatch = new SpriteBatch(GraphicsDevice);
+            this._score = new Score(Content.Load<SpriteFont>("Font"));
+            this._frameCounter = new FrameCounter(Content.Load<SpriteFont>("TimerFont"));
+            this._gamingTimer = new GamingTimer(Content.Load<SpriteFont>("TimerFont"));
+            this._menu = new Menu(
+                Content.Load<SpriteFont>("MenuFont"),
+                new Input()
+                {
+                    Up = Keys.Up,
+                    Down = Keys.Down
+                });
+
+            var leftBat =  new Bat(batTexture)
+            {
+                position = new Vector2(20, (originalScreenSize.Y / 2) - (batTexture.Height / 2)),
+                input = new Input()
+                {
+                    Up = Keys.W,
+                    Down = Keys.S,
+                }
+            };
+            var rightBat  = new Bat(batTexture)
+            {
+                position = new Vector2(originalScreenSize.X - 20 - batTexture.Width, (originalScreenSize.Y / 2) - (batTexture.Height / 2)),
+                input = new Input()
+                {
+                    Up = Keys.Up,
+                    Down = Keys.Down,
+                }
+            };
+            ball = new Ball(ballTexture)
+            {
+                position = new Vector2((originalScreenSize.X / 2) - (ballTexture.Width / 2), (originalScreenSize.Y / 2) - (ballTexture.Height / 2)),
+                score = this._score,
+                gamingTimer = this._gamingTimer,
+                pongHitTableSound = Content.Load<SoundEffect>("Sounds/Pong")
+            };
+
+            this._pongSprites = new List<PongSprite>()
+            {
+                new PongSprite(backGround),
+                leftBat,
+                rightBat,
+                ball
+            };
+            // TODO: use this.Content to load your game content here
+        }
+
+        public void ScalePresentationArea()
+        {
+            //Work out how much we need to scale our graphics to fill the screen
+            this._backbufferWidth = GraphicsDevice.PresentationParameters.BackBufferWidth;
+            this._backbufferHeight = GraphicsDevice.PresentationParameters.BackBufferHeight;
+            float horScaling = this._backbufferWidth / originalScreenSize.X;
+            float verScaling = this._backbufferHeight / originalScreenSize.Y;
+            Vector3 screenScalingFactor = new Vector3(horScaling, verScaling, 1);
+            this._globalTransformation = Matrix.CreateScale(screenScalingFactor);
+        }
+
+
+        protected override void Update(GameTime gameTime)
+        {
+            // if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            //     Exit();
+            
+            if(this._backbufferHeight != GraphicsDevice.PresentationParameters.BackBufferHeight || 
+                this._backbufferWidth!= GraphicsDevice.PresentationParameters.BackBufferWidth)
+            {
+                this.ScalePresentationArea();
+            }
+            foreach(var sprite in this._pongSprites)
+                sprite.Update(gameTime, this._pongSprites);
+            this._menu.Update(gameTime);
+            this._frameCounter.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            this._spriteBatch.Begin(SpriteSortMode.Immediate, null, null, null, null,null, this._globalTransformation);
+            foreach(var sprite in this._pongSprites)
+                sprite.Draw(this._spriteBatch);
+            this._score.Draw(this._spriteBatch);
+            this._gamingTimer.Draw(this._spriteBatch);
+            this._menu.Draw(this._spriteBatch);
+            this._frameCounter.Draw(this._spriteBatch);
+            this._spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# F1TDV
+# Pong
+
+Trabalho realizado por:
+- Gabriel Lima / 27935
+- João Melo / 
+- Henrique Brás
+
+Desenvolvido na framework: [MonoGame](https://monogame.net/)
+
+Repositório original: [Pong](https://github.com/ChrisChou-freeman/PongMonoGameExample)
+
+Ficheiros que iremos documentar: 
+
+1. Pasta Content
+2. Pasta Lib
+3. Game.cs
+4. Program.cs
+
+ 
+Pasta Content
+
+Imagens:
+
+• Background.png: Esta imagem é usada para representar o campo de jogo e tem a resolução de 800x480
+• Ball.png: É usada para representar a bola durante o jogo e tem a resolução de 16x16.
+• Bat.png: Esta imagem representa a raquete do jogador e tem a resolução de 16x100.
+
+Sons:
+• Pong.wav: Este som é usado quando a bola bate na raquete.~
+
+ Outros:
+
+• Content.mgcb: Este arquivo é usado no desenvolvimento de jogos Monogame para gerenciar e compilar ativos de conteúdo, como texturas e sons, para o jogo.
+
+• Font.spritefont: Define as características visuais de uma fonte usada no jogo, como estilo, tamanho e cor.
+
+• MenuFont.spritefont: Especifica as propriedades visuais de uma fonte específica destinada a ser usada nos menus do jogo.
+
+• TimerFont.spritefont: Define as características visuais de uma fonte usada para exibir o cronômetro dentro do jogo.
+
+
+Pasta Lib
+
+Esta pasta é destinada a armazenar bibliotecas e módulos de código reutilizáveis que são compartilhados entre vários ficheiros.
+
+• Ball.cs: 
+
